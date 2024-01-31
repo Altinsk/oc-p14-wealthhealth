7739f01d48d7461f2dba70d2c8fb0db1d230f64f
Omar Rabie <omar.rabie@hotmail.fr>
2024-01-31 20:15:43 +0100

    updated store

--- HRnet/src/pages/EmployeeList.tsx
+++ HRnet/src/pages/EmployeeList.tsx
@@ -1,61 +1,71 @@
-import DataTable from "react-data-table-component";
-import {useEffect, useMemo, useState} from "react";
-import {useSelector} from "react-redux";
-import {RootState} from "../store";
-import {tableColumns} from "../utils/tableColumns.ts";
+import DataTable from 'react-data-table-component';
+import { useEffect, useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+import { tableColumns } from '../utils/tableColumns.ts';
 
 export default function OldEmployeeList() {
-    const [filterText, setFilterText] = useState('');
-    const [resetPaginationToggle] = useState(false);
+  const [filterText, setFilterText] = useState('');
+  const [resetPaginationToggle] = useState(false);
 
-    const allEmployees = useSelector((state: RootState) => state.employee)
+  const allEmployees = useSelector((state: RootState) => state.employee);
 
-    const [employees, setEmployees] = useState([{
-        firstName: "Nebel",
-        lastName: "Mass",
-        dateOfBirth: "30/03/2000",
-        startDate: "30/03/2023",
-        city: "Brazzaville",
-        state: "Congo",
-        street: "40 rue Nkounka Batéké, Bacongo",
-        department: "Brazzaville",
-        zipCode: "00242"
-    }])
+  const [employees, setEmployees] = useState([
+    {
+      firstName: 'John',
+      lastName: 'Carlos',
+      dateOfBirth: '31/02/1999',
+      startDate: '12/09/1985',
+      city: 'Sydney',
+      state: 'Syndey',
+      street: '1 rue de Sydney, Sydney',
+      department: 'Australia',
+      zipCode: '98464',
+    },
+  ]);
 
-    useEffect(() => {
-        setEmployees(allEmployees)
-        if (filterText) {
-            setEmployees(
-                allEmployees.filter(
-                    item => item.firstName && item.firstName.toLowerCase().includes(filterText.toLowerCase())
-                )
-            )
-        }
-    }, [allEmployees, filterText]);
+  useEffect(() => {
+    setEmployees(allEmployees);
+    if (filterText) {
+      setEmployees(
+        allEmployees.filter(
+          (item) =>
+            item.firstName &&
+            item.firstName.toLowerCase().includes(filterText.toLowerCase()),
+        ),
+      );
+    }
+  }, [allEmployees, filterText]);
 
-    const subHeaderComponentMemo = useMemo(() => {
-        return <input
-            style={{border: "1px solid #ccc", borderRadius: "5px", padding: "5px 10px", marginBottom: "10px"}}
-            type={"search"}
-            onChange={(e) => setFilterText(e.target.value)}
-            value={filterText}
-            placeholder={"Search by name"}
-        />
-    }, [filterText]);
-
+  const subHeaderComponentMemo = useMemo(() => {
     return (
-        <div className="container mx-auto my-5">
-            <DataTable
-                title="Employee List"
-                columns={tableColumns}
-                data={employees}
-                pagination
-                paginationResetDefaultPage={resetPaginationToggle} // optionally, a hook to reset pagination to page 1
-                subHeader
-                subHeaderComponent={subHeaderComponentMemo}
-                persistTableHead
-            />
-
-        </div>
+      <input
+        style={{
+          border: '1px solid #ccc',
+          borderRadius: '5px',
+          padding: '5px 10px',
+          marginBottom: '10px',
+        }}
+        type={'search'}
+        onChange={(e) => setFilterText(e.target.value)}
+        value={filterText}
+        placeholder={'Search by name'}
+      />
     );
+  }, [filterText]);
+
+  return (
+    <div className="container mx-auto my-5">
+      <DataTable
+        title="Employee List"
+        columns={tableColumns}
+        data={employees}
+        pagination
+        paginationResetDefaultPage={resetPaginationToggle} // optionally, a hook to reset pagination to page 1
+        subHeader
+        subHeaderComponent={subHeaderComponentMemo}
+        persistTableHead
+      />
+    </div>
+  );
 }
