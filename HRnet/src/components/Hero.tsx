e5b9dc1b74f6bfd00622880f38cc909d2a86ed06
Omar Rabie <omar.rabie@hotmail.fr>
2024-02-02 05:36:19 +0100

    changed css hero

--- HRnet/src/components/Hero.tsx
+++ HRnet/src/components/Hero.tsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 export default function Hero() {
   return (
     <div className="w-full h-[70vh] flex justify-center space-y-6 text-center flex-col">
-      <h1 className="text-8xl font-bold bg-clip-text bg-gradient-to-r from-green-500 to-green-900 text-transparent">
+      <h1 className="text-8xl font-bold bg-clip-text bg-gradient-to-r from-green-500 to-red-900 text-transparent">
         Hello HRnet App
       </h1>
-      <p className="text-xl text-[Roboto] font-semibold">
+      <p className="text-xs text-[Roboto] font-semibold">
         Please choose an option
       </p>
       <div className="flex justify-center space-x-8">
