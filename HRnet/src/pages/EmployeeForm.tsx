cc235d2f1328ec734cfa80d59a523268ce3c49f3
Omar Rabie <omar.rabie@hotmail.fr>
2024-02-04 13:57:46 +0100

    added state as menu

--- HRnet/src/pages/EmployeeForm.tsx
+++ HRnet/src/pages/EmployeeForm.tsx
@@ -131,15 +131,88 @@ export default function EmployeeForm() {
                     />
                   </div>
                   <div className="w-full lg:w-4/12 px-4">
-                    <InputField
-                      label="State"
-                      type="text"
+                    <label className="block uppercase text-green-600 text-xs font-bold mb-2">
+                      STATE
+                    </label>
+                    <select
                       name="state"
+                      id="state"
                       value={formData.state}
                       onChange={handleChange}
                       required
-                    />
+                      className="border-0 px-3 py-3 placeholder-green-300 text-green-600
+                      bg-white rounded text-sm shadow focus:outline-none focus:ring w-full
+                      ease-linear transition-all duration-150"
+                    >
+                      <option value="">Choose state</option>
+                      <option value="Alabama">Alabama</option>
+                      <option value="Alaska">Alaska</option>
+                      <option value="American Samoa">American Samoa</option>
+                      <option value="Arizona">Arizona</option>
+                      <option value="Arkansas">Arkansas</option>
+                      <option value="California">California</option>
+                      <option value="Colorado">Colorado</option>
+                      <option value="Connecticut">Connecticut</option>
+                      <option value="Delaware">Delaware</option>
+                      <option value="District Of Columbia">
+                        District Of Columbia
+                      </option>
+                      <option value="Federated States Of Micronesia">
+                        Federated States Of Micronesia
+                      </option>
+                      <option value="Florida">Florida</option>
+                      <option value="Georgia">Georgia</option>
+                      <option value="Guam">Guam</option>
+                      <option value="Hawaii">Hawaii</option>
+                      <option value="Idaho">Idaho</option>
+                      <option value="Illinois">Illinois</option>
+                      <option value="Indiana">Indiana</option>
+                      <option value="Iowa">Iowa</option>
+                      <option value="Kansas">Kansas</option>
+                      <option value="Kentucky">Kentucky</option>
+                      <option value="Louisiana">Louisiana</option>
+                      <option value="Maine">Maine</option>
+                      <option value="Marshall Islands">Marshall Islands</option>
+                      <option value="Maryland">Maryland</option>
+                      <option value="Massachusetts">Massachusetts</option>
+                      <option value="Michigan">Michigan</option>
+                      <option value="Minnesota">Minnesota</option>
+                      <option value="Mississippi">Mississippi</option>
+                      <option value="Missouri">Missouri</option>
+                      <option value="Montana">Montana</option>
+                      <option value="Nebraska">Nebraska</option>
+                      <option value="Nevada">Nevada</option>
+                      <option value="New Hampshire">New Hampshire</option>
+                      <option value="New Jersey">New Jersey</option>
+                      <option value="New Mexico">New Mexico</option>
+                      <option value="New York">New York</option>
+                      <option value="North Carolina">North Carolina</option>
+                      <option value="North Dakota">North Dakota</option>
+                      <option value="Northern Mariana Islands">
+                        Northern Mariana Islands
+                      </option>
+                      <option value="Ohio">Ohio</option>
+                      <option value="Oklahoma">Oklahoma</option>
+                      <option value="Oregon">Oregon</option>
+                      <option value="Palau">Palau</option>
+                      <option value="Pennsylvania">Pennsylvania</option>
+                      <option value="Puerto Rico">Puerto Rico</option>
+                      <option value="Rhode Island">Rhode Island</option>
+                      <option value="South Carolina">South Carolina</option>
+                      <option value="South Dakota">South Dakota</option>
+                      <option value="Tennessee">Tennessee</option>
+                      <option value="Texas">Texas</option>
+                      <option value="Utah">Utah</option>
+                      <option value="Vermont">Vermont</option>
+                      <option value="Virgin Islands">Virgin Islands</option>
+                      <option value="Virginia">Virginia</option>
+                      <option value="Washington">Washington</option>
+                      <option value="West Virginia">West Virginia</option>
+                      <option value="Wisconsin">Wisconsin</option>
+                      <option value="Wyoming">Wyoming</option>
+                    </select>
                   </div>
+
                   <div className="w-full lg:w-4/12 px-4">
                     <InputField
                       label="Zip Code"
@@ -162,8 +235,8 @@ export default function EmployeeForm() {
                         onChange={handleChange}
                         required
                         className="border-0 px-3 py-3 placeholder-green-300 text-green-600
-                                                    bg-white rounded text-sm shadow focus:outline-none focus:ring w-full
-                                                    ease-linear transition-all duration-150"
+                        bg-white rounded text-sm shadow focus:outline-none focus:ring w-full
+                        ease-linear transition-all duration-150"
                       >
                         <option value="">Choose department</option>
                         <option value="Sales">Sales</option>
